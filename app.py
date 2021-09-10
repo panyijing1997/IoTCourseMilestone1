f37d5d4eb6e1fe758e62e1088bbc582d0300660f
Yijing Pan <202003410@post.au.dk>
2021-09-10 22:14:20 +0200

    led contro;

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(4, GPIO.OUT)  # set #4 as ouput port
+
+GPIO.output(4, GPIO.LOW)  # initially turned off
+
+
+@app.route("/")
+def index():
+    ledState = GPIO.input(4)
+    templateData = {
+        'title': 'GPIO #4 output State!',
+        'led': ledState,
+    }
+    return render_template('index.html', **templateData)
+
+
+@app.route('/led/<action>')
+def ledAction(action):
+    if action == "on":
+        GPIO.output(4, GPIO.HIGH)
+    if action == "off":
+        GPIO.output(4, GPIO.LOW)
+    ledState = GPIO.input(4)
+    templateData = {
+        'led': ledState,
+    }
+    return render_template('index.html', **templateData)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
